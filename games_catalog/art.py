027f574e043ad84ab83b56a5635219fcd7be04b3
phizzl3 <phizzl3@gmail.com>
2020-07-23 14:07:06 -0500

    Added clear screen Runs based on detected platform for MacOS and Windows.

--- games_catalog/art.py
+++ games_catalog/art.py
@@ -1,4 +1,10 @@
+import os
+import subprocess
+import platform
+
 def disp_art():
+    """Clear screen (MacOS/Windows) and display ASCII art."""
+    
     art = """  ________                              
  /  _____/_____    _____   ____   ______
 /   \  ___\__  \  /     \_/ __ \ /  ___/
@@ -6,4 +12,15 @@ def disp_art():
  \______  (____  |__|_|  /\___  /____  >
         \/     \/      \/     \/     \/ 
         """
+
+    # Clear screen based on MacOS/Win (only)
+    if platform.system() == 'Darwin':
+        subprocess.run('clear', shell=True)
+    else:
+        os.system('cls')
+    
     print(art)
+
+
+if __name__ == "__main__":
+    disp_art()
